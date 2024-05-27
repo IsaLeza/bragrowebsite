a6b41a8cb0cbd7802d9ec5b52022157553c15822
Isaias Lezama Aponte <isaiaslezama@gmail.com>
2024-05-27 11:15:13 -0600

    commit

--- src/screens/ServicesScreen.js
+++ src/screens/ServicesScreen.js
@@ -3,7 +3,7 @@ import React from "react";
 function ServicesScreen(){
     return(
         <div>
-            ServicesScreen
+            Distribuidores Screen Under Construction
         </div>
     )
 }
